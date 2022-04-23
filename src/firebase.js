8d84d3b25c4a985c59f3c298fc2d9b97a161dbb4
Raymo1892 <raymo1892@gmail.com>
2022-04-23 15:37:49 -0500

    Progress

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import "firebase/firestore";
+import { getAuth } from '@firebase/auth';
+import { initializeApp } from 'firebase/app'
+import { getFirestore, setDoc, doc } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +20,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const firestore = getFirestore();
+
+export { auth }
