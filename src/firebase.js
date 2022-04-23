6cb58f9bd8efaf9ceb19c398b68aaf41e54b923a
Raymo1892 <raymo1892@gmail.com>
2022-04-23 16:25:12 -0500

    Reset

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
-import firebase from './firebase'
+//import firebase from './firebase'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,8 +24,3 @@ const auth = getAuth(app)
 const firestore = getFirestore();
 
 export { auth }
-/*const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
-export { auth };
-export default db;*/
--- src/pages/account.js
+++ src/pages/account.js
@@ -1,95 +1,11 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./account.css";
-import { auth } from "../firebase";
-import { signInWithEmailAndPassword } from "@firebase/auth";
-import * as firebase from "../firebase";
+import React from 'react';
 
-export default function Account() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-    }
-    const auth = getAuth();
-    signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            //alert("Invalid Email/Password");
-        });
-
-    /*const Account = () => {
-        const emailRef = useRef(null);
-        const passwordRef = useRef(null);
-        const signUp = e => {
-            e.preventDefault();
-            auth.createUserWithEmailAndPassword(
-                emailRef.current.value,
-                passwordRef.current.value
-            ).then(user => {
-                console.log(user)
-            }).catch(err => {
-                console.log(err)
-            })
-        }
-        const signin = e => {
-            e.preventDefault();
-            auth.signInWithEmailAndPassword(
-                emailRef.current.value,
-                passwordRef.current.value
-            ).then(user => {
-                console.log(user)
-            }).catch(err => {
-                console.log(err)
-            })
-        }*/
+const Contact = () => {
     return (
-        <div className="Account">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group size="lg" controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        autoFocus
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder={"Enter email here"}
-                    />
-                </Form.Group>
-                <Form.Group size="lg" controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder={"Enter password here"}
-                    />
-                </Form.Group>
-                <Button block size="lg" type="submit" disabled={!validateForm()}
-                    style={{
-                        maxWidth: "100px",
-                        maxHeight: "5555px",
-                        minWidth: "234px",
-                        minHeight: "30px"
-                    }}
-                >
-                    Login
-                </Button>
-            </Form>
-        </div >
+        <div>
+            <h1>Mail us on feedback@geeksforgeeks.org</h1>
+        </div>
     );
-}
+};
 
-//export default Account;
+export default Contact;
