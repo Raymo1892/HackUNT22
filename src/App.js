c24540f1441916cd455abfe3c0c951134ee63c7c
Raymo1892 <raymo1892@gmail.com>
2022-04-23 18:34:53 -0500

    UpdateDDDD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-<<<<<<< Updated upstream
-import { BrowserRouter as Router, Routes, Route }
-  from 'react-router-dom';
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
->>>>>>> Stashed changes
 import Home from './pages';
 import About from './pages/about';
 import Account from './pages/account';
@@ -22,12 +17,7 @@ function App() {
         <Route exact path='/' exact element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-<<<<<<< Updated upstream
-        <Route path='/account' element={<Account />} />
-        <Route path='/sign-up' element={<SignUp />} />
-=======
         <Route path='/register' element={<Register />} />
->>>>>>> Stashed changes
       </Routes>
     </Router>
   );
