97800669e9b2f39ac713ff340bec94060666155f
Raymo1892 <raymo1892@gmail.com>
2022-04-24 01:46:34 -0500

    Update movie1-1.js

--- src/pages/movie1-1.js
+++ src/pages/movie1-1.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import {Seats, Invisible} from "./indexElements";
+import { Seats, Invisible } from "./indexElements";
 import { View } from "react-native";
-  
+
 
 const Movie1 = () => {
     const [cls, setCls] = useState("green");
@@ -11,19 +11,19 @@ const Movie1 = () => {
             <Invisible>Filler</Invisible>
             <hr style={{ width: "50%", size: "5", color: "black", height: "50%" }}></hr>
             <Invisible>Filler</Invisible>
-            <View style={{ flexDirection:"row"}}>
-            <Invisible>Filler</Invisible>
-            <style>{`
-                .red {color: red}
-                .green {color: green}
+            <View style={{ flexDirection: "row" }}>
+                <Invisible>Filler</Invisible>
+                <style>{`
+                .red {background-color: red}
+                .green {background-color: green}
             `}</style>
-            <Seats className={cls}onClick={() => setCls((cls) => (cls === "red" ? "green" : "red"))}>1A</Seats>
-            <Seats>2A</Seats>
-            <Invisible>Filler</Invisible>
-            <Seats>3A</Seats>
-            <Seats>4A</Seats>
-            <Seats>5A</Seats>
-            <Seats>6A</Seats>
+                <Seats className={cls} onClick={() => setCls((cls) => (cls === "red" ? "green" : "red"))}>1A</Seats>
+                <Seats className={cls} onClick={() => setCls((cls) => (cls === "red" ? "green" : "red"))}>2A</Seats>
+                <Invisible>Filler</Invisible>
+                <Seats>3A</Seats>
+                <Seats>4A</Seats>
+                <Seats>5A</Seats>
+                <Seats>6A</Seats>
             </View>
 
             <View style={{ flexDirection: "row" }}>
